cb742d66562f4e4c08101ab1cd092c4a54cf2aa1
sam <saminyaserwork@gmail.com>
2023-05-16 18:12:51 +0600

    changing to jpa

--- src/main/java/com/example/demo/repository/BlogRepository.java
+++ src/main/java/com/example/demo/repository/BlogRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 import com.example.demo.model.Blog;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //public interface BlogRepository extends JpaRepository<Blog, Long> {
@@ -13,82 +14,86 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class BlogRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public BlogRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
-    public void save(Blog blog) {
-        String sql = "INSERT INTO blog (title, content, author, date) VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, blog.getTitle(), blog.getContent(), blog.getAuthor(), blog.getDate());
-    }
-
-    public List<Blog> findAll() {
-        String sql = "SELECT * FROM blog";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> new Blog(
-                rs.getLong("id"),
-                rs.getString("title"),
-                rs.getString("content"),
-                rs.getString("author"),
-                rs.getDate("date")
-//                rs.getTimestamp("date").toLocalDateTime()
-        ));
-    }
-
-    public List<Blog> findByAuthor(String author) {
-        String sql = "SELECT * FROM blog WHERE author = ?";
-        return jdbcTemplate.query(sql, new Object[]{author}, (rs, rowNum) -> new Blog(
-                rs.getLong("id"),
-                rs.getString("title"),
-                rs.getString("content"),
-                rs.getString("author"),
-                rs.getDate("date")
-//                rs.getTimestamp("date").toLocalDateTime()
-        ));
-    }
-
-    public void update(Blog blog, Long id) {
-        String sql = "UPDATE blog SET title = ?, content = ?, author = ?, date = ? WHERE id = ?";
-        jdbcTemplate.update(sql, blog.getTitle(), blog.getContent(), blog.getAuthor(), blog.getDate(), id);
-    }
-
-    public void delete(Long id) {
-        String sql = "DELETE FROM blog WHERE id = ?";
-        jdbcTemplate.update(sql, id);
-    }
-
-    public Optional<Blog> findById(Long id) {
-        String sql = "SELECT * FROM blog WHERE id = ?";
-        List<Blog> blogs = jdbcTemplate.query(sql, new Object[]{id}, (rs, rowNum) -> new Blog(
-                rs.getLong("id"),
-                rs.getString("title"),
-                rs.getString("content"),
-                rs.getString("author"),
-                rs.getDate("date")
-//                rs.getTimestamp("date").toLocalDateTime()
-        ));
-        return blogs.isEmpty() ? Optional.empty() : Optional.of(blogs.get(0));
-    }
-
-    public void deleteById(Long id) {
-        String sql = "DELETE FROM blog WHERE id = ?";
-        jdbcTemplate.update(sql, id);
-    }
-
-    public static List<Blog> getSubList(List<Blog> data, int itemsPerPage, int page) {
-        int startIndex = (page - 1) * itemsPerPage;
-        int endIndex = Math.min(startIndex + itemsPerPage, data.size());
-        return data.subList(startIndex, endIndex);
-    }
-
-    public List<Blog> getPage(Integer page) {
-        int itemsPerPage = 10;
-        List<Blog> blogs = this.findAll();
-        return getSubList(blogs, itemsPerPage, page);
-    }
+public interface BlogRepository extends JpaRepository<User, Long> {
 }
 
-
+//@Repository
+//public class BlogRepository {
+//
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    public BlogRepository(DataSource dataSource) {
+//        this.jdbcTemplate = new JdbcTemplate(dataSource);
+//    }
+//
+//    public void save(Blog blog) {
+//        String sql = "INSERT INTO blog (title, content, author, date) VALUES (?, ?, ?, ?)";
+//        jdbcTemplate.update(sql, blog.getTitle(), blog.getContent(), blog.getAuthor(), blog.getDate());
+//    }
+//
+//    public List<Blog> findAll() {
+//        String sql = "SELECT * FROM blog";
+//        return jdbcTemplate.query(sql, (rs, rowNum) -> new Blog(
+//                rs.getLong("id"),
+//                rs.getString("title"),
+//                rs.getString("content"),
+//                rs.getString("author"),
+//                rs.getDate("date")
+////                rs.getTimestamp("date").toLocalDateTime()
+//        ));
+//    }
+//
+//    public List<Blog> findByAuthor(String author) {
+//        String sql = "SELECT * FROM blog WHERE author = ?";
+//        return jdbcTemplate.query(sql, new Object[]{author}, (rs, rowNum) -> new Blog(
+//                rs.getLong("id"),
+//                rs.getString("title"),
+//                rs.getString("content"),
+//                rs.getString("author"),
+//                rs.getDate("date")
+////                rs.getTimestamp("date").toLocalDateTime()
+//        ));
+//    }
+//
+//    public void update(Blog blog, Long id) {
+//        String sql = "UPDATE blog SET title = ?, content = ?, author = ?, date = ? WHERE id = ?";
+//        jdbcTemplate.update(sql, blog.getTitle(), blog.getContent(), blog.getAuthor(), blog.getDate(), id);
+//    }
+//
+//    public void delete(Long id) {
+//        String sql = "DELETE FROM blog WHERE id = ?";
+//        jdbcTemplate.update(sql, id);
+//    }
+//
+//    public Optional<Blog> findById(Long id) {
+//        String sql = "SELECT * FROM blog WHERE id = ?";
+//        List<Blog> blogs = jdbcTemplate.query(sql, new Object[]{id}, (rs, rowNum) -> new Blog(
+//                rs.getLong("id"),
+//                rs.getString("title"),
+//                rs.getString("content"),
+//                rs.getString("author"),
+//                rs.getDate("date")
+////                rs.getTimestamp("date").toLocalDateTime()
+//        ));
+//        return blogs.isEmpty() ? Optional.empty() : Optional.of(blogs.get(0));
+//    }
+//
+//    public void deleteById(Long id) {
+//        String sql = "DELETE FROM blog WHERE id = ?";
+//        jdbcTemplate.update(sql, id);
+//    }
+//
+//    public static List<Blog> getSubList(List<Blog> data, int itemsPerPage, int page) {
+//        int startIndex = (page - 1) * itemsPerPage;
+//        int endIndex = Math.min(startIndex + itemsPerPage, data.size());
+//        return data.subList(startIndex, endIndex);
+//    }
+//
+//    public List<Blog> getPage(Integer page) {
+//        int itemsPerPage = 10;
+//        List<Blog> blogs = this.findAll();
+//        return getSubList(blogs, itemsPerPage, page);
+//    }
+//}
+//
+//
--- src/main/java/com/example/demo/service/BlogService.java
+++ src/main/java/com/example/demo/service/BlogService.java
@@ -3,37 +3,74 @@ import java.util.List;
 import java.util.Optional;
 
 import com.example.demo.model.Blog;
+import com.example.demo.model.User;
 import com.example.demo.repository.BlogRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//@Service
+//public class BlogService {
+//
+//    @Autowired
+//    private BlogRepository blogRepository;
+//
+//    public List<Blog> getAllBlogs() {
+//        return blogRepository.findAll();
+//    }
+//
+//    public List<Blog> getAllBlogsPaging(Integer page) {
+//        return blogRepository.getPage(page);
+//    }
+//
+//    public Optional<Blog> getBlogById(Long id) {
+//        return blogRepository.findById(id);
+//    }
+//
+//    public void saveBlog(Blog blog) {
+//        blogRepository.save(blog);
+//    }
+//
+//    public void updateBlog(Blog blog, Long id) {
+//        blogRepository.update(blog, id);
+//    }
+//
+//    public void deleteBlog(Long id) {
+//        blogRepository.deleteById(id);
+//    }
+//}
+
 @Service
 public class BlogService {
-
     @Autowired
     private BlogRepository blogRepository;
 
-    public List<Blog> getAllBlogs() {
+    public List<User> getAllUsers() {
         return blogRepository.findAll();
     }
 
-    public List<Blog> getAllBlogsPaging(Integer page) {
-        return blogRepository.getPage(page);
+    public User createUser(User user) {
+        return blogRepository.save(user);
     }
 
-    public Optional<Blog> getBlogById(Long id) {
-        return blogRepository.findById(id);
-    }
-
-    public void saveBlog(Blog blog) {
-        blogRepository.save(blog);
-    }
-
-    public void updateBlog(Blog blog, Long id) {
-        blogRepository.update(blog, id);
-    }
-
-    public void deleteBlog(Long id) {
+    public void deleteUser(Long id) {
         blogRepository.deleteById(id);
+    }
+
+    public Blog updateUser(Long id, Blog blog) {
+        Blog existingBlog = blogRepository.findById(id).orElse(null);
+
+        if (existingBlog != null) {
+            existingBlog.setAuthor(blog.getAuthor());
+            existingBlog.setTitle(blog.getTitle());
+            existingBlog.setContent(blog.setContent());
+            blogRepository.save(existingBlog);
+        }
+
+        return existingBlog;
+    }
+
+    public User getUserById(Long id) {
+        return blogRepository.findById(id).orElse(null);
     }
 }
