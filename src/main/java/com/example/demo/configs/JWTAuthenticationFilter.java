1fc47bf02690add809fe44358b96df6455a2105b
Samin Yaser <saminyaserwork@gmail.com>
2023-05-25 08:00:03 +0600

    preliminary jwt implementation

---
+++ src/main/java/com/example/demo/configs/JWTAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.example.demo.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    // https://github.com/dailycodebuffer/Spring-MVC-Tutorials/blob/master/JWT-Demo/pom.xml
+
+    private JWTService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException
+    {
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7);
+        final String username = jwtService.extractUsername(jwt);
+    }
+}
---
+++ src/main/java/com/example/demo/configs/JWTService.java
@@ -0,0 +1,11 @@
+package com.example.demo.configs;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
